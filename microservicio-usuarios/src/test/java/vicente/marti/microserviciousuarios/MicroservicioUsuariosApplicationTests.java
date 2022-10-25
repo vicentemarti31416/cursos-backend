cbef49e65aa1976c6fe540279741b5dd712e0772
vicentemarti31416 <vicentemarti31416@gmail.com>
2022-10-25 14:25:55 +0200

    Initial commit

---
+++ microservicio-usuarios/src/test/java/vicente/marti/microserviciousuarios/MicroservicioUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package vicente.marti.microserviciousuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
