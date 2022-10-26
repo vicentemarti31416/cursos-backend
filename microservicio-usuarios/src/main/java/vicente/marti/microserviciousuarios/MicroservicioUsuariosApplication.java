31e1a06192dbf0541f0bf9a219fcc93bace1fd4f
vicentemarti31416 <vicentemarti31416@gmail.com>
2022-10-26 11:57:03 +0200

    Añadiendo el servidor Eureka

--- microservicio-usuarios/src/main/java/vicente/marti/microserviciousuarios/MicroservicioUsuariosApplication.java
+++ microservicio-usuarios/src/main/java/vicente/marti/microserviciousuarios/MicroservicioUsuariosApplication.java
@@ -2,8 +2,10 @@ package vicente.marti.microserviciousuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroservicioUsuariosApplication {
 
 	public static void main(String[] args) {
