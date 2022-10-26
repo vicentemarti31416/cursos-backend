79c2e486739b5040d1b03884af640428760c2a22
vicentemarti31416 <vicentemarti31416@gmail.com>
2022-10-26 12:48:28 +0200

    Añadiendo load-balancer Zuul

---
+++ microservicio-zuul/src/test/java/vicente/marti/microserviciozuul/MicroservicioZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package vicente.marti.microserviciozuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
